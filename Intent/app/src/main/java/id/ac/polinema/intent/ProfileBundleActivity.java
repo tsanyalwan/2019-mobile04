d293a92e2d223c5ea4d1d3eb407e6bb86e6351ea
ALWAN <tsanyalwan@gmail.com>
2019-10-04 03:39:49 +0700

    -pratikum melengkapi intent bundleActivity -pertanyaan menampilkan informasi data

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,12 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +16,16 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             // TODO: display value here
+            usernameText.setText(extras.getString("USERNAME_KEY"));
+            nameText.setText(extras.getString("NAME_KEY"));
+            ageText.setText(String.valueOf(extras.getInt("AGE_KEY")));
         }
     }
 }
